9ac6b42707cc82386d87b985659d1d2f553948e4
metaporia <kyahnkrafft@me.com>
2024-12-05 19:33:47 -0800

    Evil up to collection working

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+;;; Sources:
+;; basics came from:
+;; - https://arne.me/blog/emacs-from-scratch-part-one-foundations
+;;
+;; some more evil stuff from
+;; - https://systemcrafters.net/emacs-from-scratch/basics-of-emacs-configuration/
 
 ;;; Desiderata:
 ;; - org
@@ -41,8 +47,6 @@
 ;; Add user-init-file to "e register
 ;; https://stackoverflow.com/questions/12558019/shortcut-to-open-a-specific-file-in-emacs
 (set-register ?e (cons 'file "~/emacs/init.el"))
-;; from https://arne.me/blog/emacs-from-scratch-part-one-foundations
-
 
 ;;; Remove UI elements
 
@@ -90,53 +94,121 @@
 
 ;;;; Emacs
 (use-package emacs
-             :init
-             ; type y|n in confirmation dialogs
-             (defalias 'yes-or-no-p 'y-or-n-p)
-             ; utf8
-             (set-charset-priority 'unicode)
-             (setq locale-coding-system 'utf-8
-                   coding-system-for-read 'utf-8
-                   coding-system-for-write 'utf-8)
-             (set-terminal-coding-system 'utf-8)
-             (set-keyboard-coding-system 'utf-8)
-             (set-selection-coding-system 'utf-8)
-             (prefer-coding-system 'utf-8)
-             (setq default-process-coding-system '(utf-8-unix . utf-8-unix))
-             ; tabstop = 2
-             (setq-default indent-tabs-mode nil)
-             (setq-default tab-width 2)
-             ; Don't use X clipboard for unnamed register "
-             (setq select-enable-clipboard nil)
+  :init
+  ;; type y|n in confirmation dialogs
+  (defalias 'yes-or-no-p 'y-or-n-p)
 
-             ; Font
-             (set-face-attribute 'default nil :height 140)
+  ;; utf8
+  (set-charset-priority 'unicode)
+  (setq locale-coding-system 'utf-8
+        coding-system-for-read 'utf-8
+        coding-system-for-write 'utf-8)
+  (set-terminal-coding-system 'utf-8)
+  (set-keyboard-coding-system 'utf-8)
+  (set-selection-coding-system 'utf-8)
+  (prefer-coding-system 'utf-8)
+  (setq default-process-coding-system '(utf-8-unix . utf-8-unix))
 
-             ;;
+  ;; tabstop = 2
+  (setq-default indent-tabs-mode nil)
+  (setq-default tab-width 2)
+  
+  ;; Don't use X clipboard for unnamed register "
+  (setq select-enable-clipboard nil)
+  
+  ;; Font
+  (set-face-attribute 'default nil :height 140)
 
-             )
+  ;; Save minibuffer history
+  (setq history-length 25)
+  (savehist-mode t)
+
+  ;; Remember cursor position in files
+  (save-place-mode t)
+
+  ;; disable pop-up confirmation box
+  (setq use-dialog-box nil)
+
+  
+  ;; Revert buffers when the underlying file has changed
+  (global-auto-revert-mode 1)
+
+  )
 
 
 ;;;; Evil
 
-(use-package evil
-             :demand ; No lazy-loading
-             :config
-             (evil-mode 1)
+(use-package evil-collection
+  :after evil
+  :demand
+  :config
+  (evil-collection-init)
+  )
 
-             )
+(use-package evil
+  :demand ; No lazy-loading
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-shift-width 2)
+  :config
+  (evil-mode 1)
+
+  ;; map c-g to escape or keyboard-quit (default)
+  ;;
+  ;; https://emacs.stackexchange.com/questions/13763/how-can-i-make-c-g-run-both-evil-force-normal-state-and-keyboard-quit
+  (defun evil-keyboard-quit ()
+    "Keyboard quit and force normal state."
+    (interactive)
+    (and evil-mode (evil-force-normal-state))
+    (keyboard-quit))
+
+  (define-key evil-normal-state-map   (kbd "C-g") #'evil-keyboard-quit) 
+  (define-key evil-motion-state-map   (kbd "C-g") #'evil-keyboard-quit) 
+  (define-key evil-insert-state-map   (kbd "C-g") #'evil-keyboard-quit) 
+  (define-key evil-window-map         (kbd "C-g") #'evil-keyboard-quit) 
+  (define-key evil-operator-state-map (kbd "C-g") #'evil-keyboard-quit) 
+
+  ;; Leader maps
+
+  ;; Global leader
+  (defvar leader-map (make-sparse-keymap)
+    "Keymap for <leader> binds")
+
+  ;; free ',' for leader-map
+  (define-key evil-motion-state-map (kbd "\\") 'evil-repeat-find-char-reverse)
+
+  ;; enter 'leader-map'
+  (define-key evil-normal-state-map "," leader-map)
+
+  (evil-define-key nil leader-map
+    "e" 'eval-last-sexp
+    "w" 'save-buffer
+    "b" 'list-buffers
+    )
+
+  ;; Local Leader
+  (defvar local-leader-map (make-sparse-keymap)
+    "Keymap for <local-leader> binds")
+
+  ;; enter 'local-leader-map'
+  (define-key evil-normal-state-map (kbd "SPC") local-leader-map)
+
+  ;; TODO:
+  ;; - allow 'q' to quit eg help popups without having to leave normal mode
+  ;;   (macros are unnecessary outside of proper file buffers)
+  ;;   - evil-collection should do the trick
+  ;; - evil: swap ',' and '\' to free up ',' for leader
+  )
 
 (use-package evil-escape
-  ; :after evil
-  ; :ensure t
+  :demand
   :config
   (evil-escape-mode)
   (setq-default evil-escape-key-sequence "jk"))
 
-
-
-
-;;; Themes
+;;;; Theme
 
 (use-package doom-themes
   :demand
@@ -144,7 +216,7 @@
   (load-theme 'doom-nord)
   )
 
-;;; Relative Line Numbers
+;;;; Relative Line Numbers
 
 (use-package emacs
   :init
@@ -153,7 +225,7 @@
   )
 
 
-;;; Modeline
+;;;; Modeline
 
 (use-package doom-modeline
   :demand
@@ -164,7 +236,7 @@
 (use-package nerd-icons)
 
 
-;;; Helpful (better help w/ e.g., C-h k)
+;;;; Helpful (better help w/ e.g., C-h k)
 
 (use-package helpful
   :bind (

98dfe7f9735d4fd5b0dbe7ac4c35b8d3b4195fa4
metaporia <kyahnkrafft@me.com>
2025-01-09 20:43:39 -0800

    Tiny tweaks.

--- init.el
+++ init.el
@@ -3,49 +3,17 @@
 ;; - https://arne.me/blog/emacs-from-scratch-part-one-foundations
 ;;
 ;; some more evil stuff from
-;; - https://systemcrafters.net/emacs-from-scratch/basics-of-emacs-configuration/
 
-;;; Desiderata:
-;; - org
-;; - evil
-;; - completion (corfu)
-;; - telescope replacement (helm looks good)
-
-;; Packages to consider
-
-;; - desktop (sessions, I think)
-
-;;;; TODO
-;; - outline mode (for elisp)
-;; - bookmarks/session/common files in registers
-
-;;;; Load =.custom.el=
-
-;; So injected settings don't muck up our manually managed settings we
-;; store them elsewhere
-
-(setq-default custom-file (expand-file-name "custom.el" user-emacs-directory))
-(when (file-exists-p custom-file)
-  (load custom-file))
-
-;;;; Meta: reload config
-(defun reload-emacs-config ()
-  "
-  Reloads ~/.config/emacs/init.el
-
-  NB: 
-  - not idempotent
-  - skips early-init.el
-  "
-  (interactive)
-  (load-file user-init-file))
-(global-set-key (kbd "C-c r") 'reload-emacs-config)
-
-
-
-;; Add user-init-file to "e register
-;; https://stackoverflow.com/questions/12558019/shortcut-to-open-a-specific-file-in-emacs
+;; - https://systemcrafters.net/emacs-from-erflow.com/questions/12558019/shortcut-to-open-a-specific-file-in-emacs
 (set-register ?e (cons 'file "~/emacs/init.el"))
+
+;;; Misc helpers
+(defun load-user-file (file)
+  (interactive "f")
+  "Load a file in current user's configuration directory"
+  (load-file (expand-file-name file user-emacs-directory)))
+
+;;(define-key nil (kbd "C-c r") (load-file (expand-file-name "init.el" user-emacs-directory)))
 
 ;;; Remove UI elements
 
@@ -409,90 +377,7 @@
    '((file (styles basic partial-completion))))
   )
 
-;; TODO: bind to transfer completion to minibuffer
-(use-package corfu
-  :after evil evil-collection
-  :demand
-  :custom
-  ;; Enable cycling for `corfu-next/previous'
-  (corfu-cycle t)
-  (corfu-count 14)
-  (corfu-scroll-margin 6)
-  (corfu-preselect-first t)
-  (global-corfu-minibuffer nil)
-  
-                                        ; (corfu-preview-current t)
-  :init
-  (corfu-popupinfo-mode) ;; show doc previews
-  (global-corfu-mode)
-
-  ;; unbind org tab
-  :config
-  (evil-define-key 'insert 'global 
-    (kbd "C-n")  'completion-at-point)
-  (evil-define-key 'insert 'corfu-map
-    (kbd "C-e")  'corfu-complete
-                                        ;(kbd "TAB") 'corfu-next
-    (kbd "C-y")  'corfu-quit)
-
-
-  ;; TODO: map tab to corfu-next in commandline
-
-  ;; FIXME
-  ;; (evil-define-key 'insert 'evil-ex-completion-map (kbd "TAB")
-  ;;             (lambda (&optional _)
-  ;;                (interactive) (message "hello")))
-  ;; `(menu-item "" nil :filter
-  ;;             ,(lambda (&optional _)
-  ;;               (interactive) (message "hello") )))
-                                        ; #'corfu-next)))
-
-  ;;(define-key evil-command-line-map (kbd "C-n")  'corfu-next)
-  ;;(define-key evil-command-line-map (kbd "C-p")  'corfu-previous)
-  ;;(define-key evil-command-line-map (kbd "C-y")  'corfu-quit)
-
-  ;; these may be necessary for org-mode
-  ;;(evil-define-key 'insert 'org-mode-map (kbd "TAB") nil)
-  ;;(evil-define-key nil 'evil-insert-state-map (kbd "TAB") nil)
-  ;;(evil-define-key 'insert 'evil-org-mode (kbd "TAB") #'completion-at-point)
-  ;;(define-key evil-insert-state-map (kbd "C-n")  #'completion-at-point)
-  ;;(define-key evil-insert-state-map (kbd "C-e")  #'corfu-complete)
-
-
-  )
-
-;; Add extensions
-(use-package cape
-  :demand
-  ;; Bind prefix keymap providing all Cape commands under a mnemonic key.
-  ;; Press C-c p ? to for help.
-  :bind ("C-c p" . cape-prefix-map) ;; Alternative keys: M-p, M-+, ...
-  ;; Alternatively bind Cape commands individually.
-  ;; :bind (("C-c p d" . cape-dabbrev)
-  ;;        ("C-c p h" . cape-history)
-  ;;        ("C-c p f" . cape-file)
-  ;;        ...)
-  :init
-  ;; Add to the global default value of `completion-at-point-functions' which is
-  ;; used by `completion-at-point'.  The order of the functions matters, the
-  ;; first function returning a result wins.  Note that the list of buffer-local
-  ;; completion functions takes precedence over the global list.
-  (add-hook 'completion-at-point-functions #'cape-dabbrev)
-  (add-hook 'completion-at-point-functions #'cape-file)
-  (add-hook 'completion-at-point-functions #'cape-elisp-block)
-  ;; (add-hook 'completion-at-point-functions #'cape-history)
-  ;; ...
-  )
-
-;; TODO: set completion-styles-alist (try 'flex')
-(use-package kind-icon
-  :demand
-  :after corfu
-  :custom
-  (kind-icon-blend-background t)
-  (kind-icon-default-face 'corfu-default) ; only needed with blend-background
-  :config
-  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+(load-user-file "./completion.el")
 
 
 ;;;; Which-key
@@ -1013,3 +898,16 @@
   ;;(setq completion-category-overrides '(buffer . '(
   )
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("34cf3305b35e3a8132a0b1bdf2c67623bc2cb05b125f8d7d26bd51fd16d547ec" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
